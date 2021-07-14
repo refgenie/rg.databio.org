77219b2732fde9bb387a563a2a641ddeaddb78e5
stolarczyk <stolarczyk.michal93@gmail.com>
2021-07-14 09:34:30 -0400

    snakemake workflow updates: - make derived level1 and 2 assets building independent - automatically restrict the set of assets to build based on the PEP

--- snakemake_workflow/snakemake_helper_functions.py
+++ snakemake_workflow/snakemake_helper_functions.py
@@ -1,18 +1,9 @@
 import os
+from refgenie.asset_build_packages import *
 
-from refgenie.asset_build_packages import asset_build_packages
-
-DESC = "description"
-ASSET_DESC = "asset_description"
-ASSETS = "assets"
-PTH = "path"
-REQ_FILES = "required_files"
-REQ_ASSETS = "required_assets"
-REQ_PARAMS = "required_parameters"
-CONT = "container"
-CMD_LST = "command_list"
-KEY = "key"
-DEFAULT = "default"
+INPUT_TEMPLATE = os.path.join(
+    os.environ.get("REFGENIE_RAW"), "{genome}-{asset}-{input_id}"
+)
 
 
 def get_req_by_asset(req_type):
@@ -34,16 +25,36 @@ def get_input_templates(wildcards, asset=None):
     a = asset or wildcards.asset
     if isinstance(a, str):
         a = [a]
-    input_files_dir = os.environ.get(
-        "REFGENIE_RAW", "/Users/mstolarczyk/Desktop/testing/refgenie/raw"
-    )
-    input_file_template = "{genome}-{asset}-{input_id}"
-    input_template = os.path.join(input_files_dir, input_file_template)
     nested_list = [
         [
-            input_template.format(genome=wildcards.genome, asset=a, input_id=file_id)
+            INPUT_TEMPLATE.format(genome=wildcards.genome, asset=a, input_id=file_id)
             for file_id in get_req_files_by_asset()[a]
         ]
         for a in a
     ]
     return [item for sublist in nested_list for item in sublist]
+
+
+def get_asset_file_spec(wildcards, asset=None):
+    return [
+        "{}={}".format(
+            file_id,
+            INPUT_TEMPLATE.format(
+                genome=wildcards.genome,
+                asset=asset or wildcards.asset,
+                input_id=file_id,
+            ),
+        )
+        for file_id in get_req_files_by_asset()[asset or wildcards.asset]
+    ]
+
+
+def get_pep_intersect_for_genome(genome, recipe_list, project, exclusion_list=[]):
+    genome_samples = [sample for sample in project.samples if sample.genome == genome]
+    # this assumes that there is a 1:1 mapping between asset and recipe names
+    # this is the case for now, but in the future we may want to use asset classes somehow
+    return [
+        sample.asset
+        for sample in genome_samples
+        if sample.asset in recipe_list and sample.asset not in exclusion_list
+    ]
